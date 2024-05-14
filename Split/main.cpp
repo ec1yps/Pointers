343e1323d379ed5819d03c9ef8753954f2d56354
ec1yps_ <Mihli_sk06@top-academy.ru>
2024-05-14 20:49:02 +0300

    Split DONE

--- Split/main.cpp
+++ Split/main.cpp
@@ -3,15 +3,48 @@ using namespace std;
 
 #define tab "\t"
 
-int e_size = 0, o_size = 0;
+//#define HOMEWORK
+
+//int e_size = 0, o_size = 0;	VERY BAD!!!!!!!!!!!!!!!
 
 void FillRand(int arr[], const int n);
 void Print(int arr[], const int n);
-void Split(int arr[], int arr_chet[], int arr_nech[], const int n);
+//void Split(int arr[], int arr_chet[], int arr_nech[], const int n);
 
 void main()
 {
 	setlocale(LC_ALL, "");
+	const int n = 10;
+	int arr[n];
+	FillRand(arr, n);
+	Print(arr, n);
+
+	int even_count = 0;
+	int odd_count = 0;
+	for (int i = 0; i < n; i++)
+	{
+		//if (arr[i] % 2 == 0) even_count++;
+		//else odd_count++;
+		arr[i] % 2 == 0 ? even_count++ : odd_count++;
+	}
+	cout << "Количество четных элементов: " << even_count << endl;
+	cout << "Количество нечетных элементов: " << odd_count << endl;
+
+	int* even_arr = new int[even_count];
+	int* odd_arr = new int[odd_count];
+
+	for (int i = 0, j = 0, k = 0; i < n; i++)
+	{
+		//if (arr[i] % 2 == 0) even_arr[j++] = arr[i];
+		//else odd_arr[k++] = arr[i];
+		//arr[i] % 2 == 0 ? even_arr[j++] = arr[i] : odd_arr[k++] = arr[i];
+		(arr[i] % 2 == 0 ? even_arr[j++] : odd_arr[k++]) = arr[i];
+	}
+
+	Print(even_arr, even_count);
+	Print(odd_arr, odd_count);
+
+#ifdef HOMEWORK
 	int n = 10;
 	int* arr = new int[n];
 	int* even = new int[n];
@@ -27,6 +60,7 @@ void main()
 	delete[] arr;
 	delete[] even;
 	delete[] odd;
+#endif // HOMEWORK
 }
 
 void FillRand(int arr[], const int n)
@@ -41,12 +75,12 @@ void Print(int arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
-		cout << tab << arr[i];
+		cout << arr[i] << tab;
 	}
 	cout << endl;
 }
 
-void Split(int arr[], int arr_chet[], int arr_nech[], const int n)
+/*void Split(int arr[], int arr_chet[], int arr_nech[], const int n)
 {
 	int chet = 0, nech = 0;
 	for (int i = 0; i < n; i++)
@@ -70,4 +104,4 @@ void Split(int arr[], int arr_chet[], int arr_nech[], const int n)
 	}
 	e_size = chet;
 	o_size = nech;
-}
+}*/
